588c1b577f2f336621d5b18ecf57443338cc7ce9
EPMC-STC-CICD <63232604+EPMC-STC-CICD@users.noreply.github.com>
2020-05-22 15:22:08 +0300

    Update Get-Artifacts.ps1

--- automation/Get-Artifacts.ps1
+++ automation/Get-Artifacts.ps1
@@ -8,7 +8,10 @@ ForEach ($Folder in $SubFoldersList) {
 	$ArtifactSourcePath = Join-Path $Folder code
 	robocopy $ArtifactSourcePath/bin $ArtifactDestinationLayer/bin HCA.$ArtifactName.*
 	robocopy $ArtifactSourcePath/App_Config $ArtifactDestinationLayer/App_Config /s
-	robocopy $ArtifactSourcePath/dist $ArtifactDestinationLayer/dist /s
 	robocopy $ArtifactSourcePath/Views $ArtifactDestinationLayer/Views /s
+	if ($Folder -match "Project") {
+		$DistArtifactSourcePath = Join-Path $Folder client/build
+		robocopy $DistArtifactSourcePath $ArtifactDestinationLayer/dist /s
+		}
 	}
 robocopy $ArtifactSource $ArtifactDestination/unicorn/$ArtifactName *.yml /s

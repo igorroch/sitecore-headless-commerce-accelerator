d564e3c9a7ad66bce9f444bbd90bc3dc8c07f1cf
EPMC-STC-CICD <63232604+EPMC-STC-CICD@users.noreply.github.com>
2020-05-22 12:19:47 +0300

    Update Get-Artifacts.ps1

--- automation/Get-Artifacts.ps1
+++ automation/Get-Artifacts.ps1
@@ -1,12 +1,14 @@
 $ArtifactName = $args[0]
 $ArtifactSource = $(Pipeline.Workspace)/s/src/$ArtifactName
-$ArtifactDestinationPathPath = $(Pipeline.Workspace)/s/output/$ArtifactName
+$ArtifactDestination = $(Pipeline.Workspace)/s/output
+$ArtifactDestinationLayer = $ArtifactDestination/$ArtifactName
 $SubFoldersList = dir $ArtifactSource | Where-Object {$_.PSIsContainer} | ForEach-Object -Process {$_.FullName}
 
 ForEach ($Folder in $SubFoldersList) {
 	$ArtifactSourcePath = Join-Path $Folder code
-	robocopy $ArtifactSourcePath/bin $ArtifactDestinationPath/bin HCA.$ArtifactName.*
-	robocopy $ArtifactSourcePath/App_Config $ArtifactDestinationPath/App_Config /s
-	robocopy $ArtifactSourcePath/dist $ArtifactDestinationPath/dist /s
-	robocopy $ArtifactSourcePath/Views $ArtifactDestinationPath/Views /s
+	robocopy $ArtifactSourcePath/bin $ArtifactDestinationLayer/bin HCA.$ArtifactName.*
+	robocopy $ArtifactSourcePath/App_Config $ArtifactDestinationLayer/App_Config /s
+	robocopy $ArtifactSourcePath/dist $ArtifactDestinationLayer/dist /s
+	robocopy $ArtifactSourcePath/Views $ArtifactDestinationLayer/Views /s
 	}
+robocopy $ArtifactSource $ArtifactDestination/unicorn/$ArtifactName *.yml /s

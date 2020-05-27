c8908b253094ed4b8ef49ac957e22c4db82ca739
EPMC-STC-CICD <63232604+EPMC-STC-CICD@users.noreply.github.com>
2020-05-27 12:21:20 +0300

    Update Get-Artifacts.ps1

--- automation/Get-Artifacts.ps1
+++ automation/Get-Artifacts.ps1
@@ -6,6 +6,6 @@ $SubFoldersList = dir $ArtifactSource | Where-Object {$_.PSIsContainer} | ForEac
 
 ForEach ($Folder in $SubFoldersList) {
 	$ArtifactSourcePath = Join-Path $Folder code/obj/Debug/Package/PackageTmp
-	robocopy $ArtifactSourcePath $ArtifactDestinationLayer
+	robocopy $ArtifactSourcePath $ArtifactDestinationLayer /s /xo
 	}
 robocopy $ArtifactSource $ArtifactDestination/unicorn/$ArtifactName *.yml /s
